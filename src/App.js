cd3d09a23f7a123ff26144319abca5374b008227
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-04-12 12:45:54 -0600

    added useStates

--- src/App.js
+++ src/App.js
@@ -24,9 +24,13 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+
+  }
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
